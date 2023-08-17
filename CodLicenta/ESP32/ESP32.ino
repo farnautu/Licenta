969af93ac29e8c3ff9cd181f810358470218deef
farnautu <farnautu@rocketmail.com>
2023-08-17 20:58:43 +0300

    Version3.0

--- CodLicenta/ESP32/ESP32.ino
+++ CodLicenta/ESP32/ESP32.ino
@@ -1,11 +1,7 @@
-// ESP32 dual-core example
-// Gary Sims October 2022
+#include <WiFi.h>
 
-// ESP32: Arduino code will run on which core is selected under
-// "Arduino Runs On" menu
-// If the program doesn't do any Serial I/O or call delay then
-// it should occasionally call vTaskDelay(1) to stop
-// the watchdog from triggering. yield() doesn't seem to work.
+const char* ssid = "ASUS_5G";
+const char* password = "AsusNokia100"; 
 
 TaskHandle_t task_loop1;
 void esploop1(void* pvParameters) {
@@ -15,15 +11,9 @@ void esploop1(void* pvParameters) {
     loop1();
 }
 
-int count0, count1;
-
 void setup() {
 
-  Serial.begin(115200);
-  while (!Serial) {
-    delay(1);  // wait for serial port to connect.
-  }
-
+ 
   xTaskCreatePinnedToCore(
     esploop1,               /* Task function. */
     "loop1",                /* name of task. */
@@ -33,25 +23,34 @@ void setup() {
     &task_loop1,            /* Task handle to keep track of created task */
     !ARDUINO_RUNNING_CORE); /* pin task to core */
 
-  count0 = 0;
 }
 
 void setup1() {
-  count1 = 1000;
+   Serial.begin(115200);
+  delay(1000);
+  WiFi.begin(ssid, password);
+  while (!Serial) {
+    delay(1);  // wait for serial port to connect.
+  }
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: " + WiFi.localIP().toString());
 }
 
 void loop() {
-  Serial.printf("loop%d(): %d\n", xPortGetCoreID(), count0);
-  count0++;
-  if (count0 > 1000)
-    count0 = 0;
-  delay(1000);
+  //Serial.printf("loop%d(): %d\n", xPortGetCoreID(), count0);
+  delay(50);
 }
 
 void loop1() {
-  Serial.printf("loop%d(): %d\n", xPortGetCoreID(), count1);
-  count1--;
-  if (count1 <= 0)
-    count1 = 1000;
-  delay(2500);
+  //Serial.printf("loop%d(): %d\n", xPortGetCoreID(), count1);
+  delay(50);
 }
